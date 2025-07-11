2953f22eb611a43618eca0fd42edc113098d40a8
Urazmaxambetov Serik <urazmaxambetovserik@gmail.com>
2025-07-11 09:28:59 +0500

    feat(backend): implement controller and connect microservices

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,20 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload, Transport } from '@nestjs/microservices';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @MessagePattern('some', Transport.NATS)
+  natsSome(@Payload() payload: object) {
+    return { transport: 'nats', payload };
+  }
+
+  @MessagePattern('some', Transport.REDIS)
+  redisSome(@Payload() payload: object) {
+    return { transport: 'redis', payload };
+  }
+
+  @MessagePattern('some', Transport.RMQ)
+  rmqSome(@Payload() payload: object) {
+    return { transport: 'rmq', payload };
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: { urls: ['amqp://localhost:5672'] },
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: { host: 'localhost', port: 6379 },
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: { servers: ['nats://localhost:4222'] },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();

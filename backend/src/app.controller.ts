7072fe251ee690061a039ed17fbfb5f8e0552d8e
Urazmaxambetov Serik <urazmaxambetovserik@gmail.com>
2025-07-11 10:41:44 +0500

    feat(backend): add events and utils for events tests

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,15 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload, Transport } from '@nestjs/microservices';
+import {
+  EventPattern,
+  MessagePattern,
+  Payload,
+  Transport,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
+  private events: { transport: string; payload: object }[] = [];
+
   @MessagePattern('some', Transport.NATS)
   natsSome(@Payload() payload: object) {
     return { transport: 'nats', payload };
@@ -16,5 +23,32 @@ export class AppController {
   @MessagePattern('some', Transport.RMQ)
   rmqSome(@Payload() payload: object) {
     return { transport: 'rmq', payload };
+  }
+
+  @EventPattern('event', Transport.NATS)
+  natsEvent(@Payload() payload: object) {
+    this.events.push({ transport: 'nats', payload });
+  }
+
+  @EventPattern('event', Transport.REDIS)
+  redisEvent(@Payload() payload: object) {
+    this.events.push({ transport: 'redis', payload });
+  }
+
+  @EventPattern('event', Transport.RMQ)
+  rmqEvent(@Payload() payload: object) {
+    this.events.push({ transport: 'rmq', payload });
+  }
+
+  // For test events
+  @MessagePattern('clear_events')
+  clearEvents() {
+    this.events = [];
+    return {};
+  }
+
+  @MessagePattern('get_events')
+  getEvents(@Payload() payload: { transport: string }) {
+    return this.events.filter((e) => e.transport === payload.transport);
   }
 }

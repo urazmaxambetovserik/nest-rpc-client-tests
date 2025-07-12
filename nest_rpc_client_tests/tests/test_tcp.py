18b588adee4b7b72ceffca26e43772481a6869d3
Urazmaxambetov Serik <urazmaxambetovserik@gmail.com>
2025-07-12 13:26:45 +0500

    test: add tcp transport tests

---
+++ nest_rpc_client_tests/tests/test_tcp.py
@@ -0,0 +1,33 @@
+import pytest
+from nest_rpc_client.client import Client
+from nest_rpc_client.config.tcp import TCPConfig
+from nest_rpc_client.transports.tcp import TCPTransport
+
+
+@pytest.fixture
+def client():
+    config = TCPConfig("localhost", 3002)
+    transport = TCPTransport(config)
+    return Client(transport)
+
+
+@pytest.mark.asyncio
+async def test_redis_send(client: Client):
+    async with client:
+        some_very_big_text = "some" * 100000
+        response = await client.send("some", {"some": some_very_big_text})
+        assert response["transport"] == "tcp"
+        assert response["payload"] == {"some": some_very_big_text}
+
+
+@pytest.mark.asyncio
+async def test_redis_emit(client: Client):
+    async with client:
+        response = await client.emit("event", {"some": "data to redis"})
+        assert response is None
+
+        sent_events = await client.send("get_events", {"transport": "tcp"})
+        assert len(sent_events) == 1
+        assert sent_events[0]["payload"] == {"some": "data to redis"}
+
+        await client.send("clear_events", {})
